23281608939f0ec59199bc6f8918b770a2aeb403
Ekarat <ekarat.stw@gmail.com>
2021-05-25 15:28:49 +0700

    added notes.

---
+++ Fixed-wing/vehicle_state/pitch_angle_state.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+© Copyright 2015-2016, 3D Robotics.
+vehicle_state.py: 
+
+Demonstrates how to get and set vehicle state and parameter information, 
+and how to observe vehicle attribute (state) changes.
+
+Full documentation is provided at http://python.dronekit.io/examples/vehicle_state.html
+"""
+from __future__ import print_function
+from dronekit import connect, VehicleMode
+import time
+
+#Set up option parsing to get connection string
+import argparse  
+
+
+
+connection_string = "/dev/ttyACM0"
+baud_rate = 57600
+
+
+#Start SITL if no connection string specified
+if not connection_string:
+    import dronekit_sitl
+    sitl = dronekit_sitl.start_default()
+    connection_string = sitl.connection_string()
+
+
+# Connect to the Vehicle. 
+#   Set `wait_ready=True` to ensure default attributes are populated before `connect()` returns.
+print('Connecting to Vehicle on: %s' %connection_string)
+vehicle = connect(connection_string, baud=baud_rate, wait_ready=True)
+
+vehicle.wait_ready('autopilot_version')
+
+# Get all vehicle attributes (state)
+print("\nGet all vehicle attribute values:")
+print " Pitch in Euler Angle : %f " % float(vehicle.attitude.pitch*const)
