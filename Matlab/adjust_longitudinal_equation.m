d1d4ccc9bb8cd110b23aff257a26f0ddd8c2195e
Ekarat <ekarat.stw@gmail.com>
2021-08-17 11:02:39 +0700

    edited Cm.

--- Matlab/adjust_longitudinal_equation.m
+++ Matlab/adjust_longitudinal_equation.m
@@ -62,10 +62,19 @@ function dy = adjust_longitudinal_equation(t, y, deepstall_angle, adjust_angle, 
     sigma = (1 + exp(-M*(alpha-alpha0))+ exp(M*(alpha+alpha0))) / ((1+exp(-M*(alpha-alpha0))) * (1+exp(M*(alpha+alpha0))));
     CL = (1-sigma)*(CL0+CLalpha*alpha) + sigma*(2*sign(alpha)*((sin(alpha))^2)*cos(alpha));
     CD = CD0 + (1-sigma)*K*((CL0+CLalpha*alpha)^2) + sigma*(2*sign(alpha)*((sin(alpha))^3));
-    CM = CM0 + CMalpha*alpha;
+%     CM = CM0 + CMalpha*alpha;
+    CM = M / (0.5*p*(V^2)*S*c);
+  
     L = 0.5*p*S*(V^2)*(CL + ((CLq*c*q)/(2*V)) + CLdelta*delta);
     D = 0.5*p*S*(V^2)*(CD + ((CDq*c*q)/(2*V)) + CDdelta*delta);
     M = 0.5*p*S*(V^2)*c*(CM + ((CMq*c*q)/(2*V)) + CMdelta*delta);
+    Se = 0.041;
+    le = -0.5;
+    lw = 0.025;
+%     Me = -0.5*p*(V^2)*Se*le*( (0.8*cos(2*alpha+2*delta)) + (1.4*sin(alpha)*(sin(alpha+delta))^2) + 0.1*sin(alpha) );
+%     Mw = -lw*(L*cos(alpha) + D*sin(alpha));
+%     M = Me + Mw;
+
  
     dy(1) = (T*cos(alpha) - D - m*g*sin(gramma)) / m;
     dy(2) = (T*sin(alpha) + L - m*g*cos(gramma)) / (m*V);
